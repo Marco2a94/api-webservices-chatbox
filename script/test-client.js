50d0fc5326bad1c49c3bb7489c15d75b0131214b
Marco2a94 <marco.arrighi@efrei.net>
2025-06-11 17:35:34 +0200

    Add test client for Socket.io chat functionality

---
+++ script/test-client.js
@@ -0,0 +1,16 @@
+const { io } = require("socket.io-client");
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+    console.log("Connected to server");
+
+    socket.emit("chat message", {
+        username: "Marco",
+        message: "Salut Thomas !"
+    });
+});
+
+socket.on("chat message", (data) => {
+    console.log("Nouveau message reçu", data);
+});
